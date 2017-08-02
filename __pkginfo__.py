34aa8dc92cdc2bec54295371807c4c08258c5f82
rocky <rocky@gnu.org>
2017-08-02 12:12:51 -0400

    Bump copyright

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009, 2010, 2013, 2015 Rocky Bernstein <rocky@gnu.org>
+# Copyright (C) 2009, 2010, 2013, 2015, 2017 Rocky Bernstein <rocky@gnu.org>
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
