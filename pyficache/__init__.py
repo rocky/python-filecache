34d9022f3245cda5dff6197862c3d0757c5055e6
rocky <rocky@gnu.org>
2018-06-26 09:08:39 -0400

    Add main exports

--- pyficache/__init__.py
+++ pyficache/__init__.py
@@ -92,3 +92,5 @@ License:  Copyright (c) 2009, 2015 Rocky Bernstein. Released under the GNU GPL 3
    :target: https://pypi.python.org/pypi/pyficache/
 """
 __docformat__ = 'restructuredtext'
+
+from pyficache.main import PYTHON3, PYVER, cache_file, cache_script, cached_files, checkcache, clear_file_cache, clear_file_format_cache, dark_terminal_formatter, file_cache, file2file_remap, getline, getlines, highlight_array, highlight_string, is_cached, is_cached_script, is_mapped_file, light_terminal_formatter, maxline, path, pyc2py, remap_file, remap_file_lines, remove_remap_file, sha1, size, stat, terminal_256_formatter, trace_line_numbers, uncache_script, unmap_file, unmap_file_line, update_cache, update_script_cache # NOQA
