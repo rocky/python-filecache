a113223487217494cdd17966be4c037d3cad824c
rocky <rocky@gnu.org>
2020-05-22 03:47:50 -0400

    Administrivia

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 PYTHON_VERSION=2.4.6
 
-if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
+owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+
 (cd ../python-xdis && git checkout python-2.4 && pyenv local $PYTHON_VERSION) && git pull && \
     git checkout python-2.4  && git pull && pyenv local $PYTHON_VERSION
+
+cd $owd
+rm -v */.python-version || true
