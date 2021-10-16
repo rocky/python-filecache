f034df5f3567e49708aeafeae80c47b0f0d6f2be
rocky <rocky@gnu.org>
2021-10-15 22:41:40 -0400

    Testing administrivia

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -12,7 +12,7 @@ mydir=$(dirname $bs)
 fulldir=$(readlink -f $mydir)
 cd $fulldir/..
 
-(cd ../python-xdis && git checkout python-2.4 && pyenv local $PYTHON_VERSION) && git pull && \
+(cd ../python-xdis && git checkout python-2.4-to-2.7 && pyenv local $PYTHON_VERSION) && git pull && \
     git checkout python-2.4  && git pull && pyenv local $PYTHON_VERSION
 
 cd $owd
