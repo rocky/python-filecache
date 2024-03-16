1881e9e29b1873f9552b355545ed1618fd49d607
rocky <rocky@gnu.org>
2024-03-16 10:09:24 -0400

    Admnistrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -2,7 +2,7 @@
 PYTHON_VERSION=3.8.18
 
 export PATH=$HOME/.pyenv/bin/pyenv:$PATH
-pyficache_master_owd=$(pwd)
+setup_filecache_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
@@ -15,5 +15,5 @@ cd $fulldir/..
 
 (cd ../python-xdis && git checkout master && pyenv local $PYTHON_VERSION) && git pull && \
     git checkout master && git pull && pyenv local $PYTHON_VERSION
-cd $pyficache_master_owd
+cd $setup_filecache_owd
 rm -v */.python-version || true
