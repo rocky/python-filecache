2c24dbcb9cf8f400148d120630d48f7df7a3e5db
rocky <rocky@gnu.org>
2024-10-16 09:52:53 -0400

    Need full setup on Python 3.6-3.10

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 """
-setuptools setup (setup.py) for pyficach
+distutils setup (setup.py)
 
 This gets a bit of package info from __pkginfo__.py file
 """
-
 # Get the required package information
+from __pkginfo__ import (
+    author,
+    author_email,
+    classifiers,
+    install_requires,
+    license,
+    long_description,
+    modname,
+    py_modules,
+    short_desc,
+    __version__,
+    web,
+    zip_safe,
+)
 
+from setuptools import setup, find_packages
 
-from setuptools import setup
+__import__("pkg_resources")
 
-setup(packages=["pyficache"])
+packages = find_packages()
+
+setup(
+    author=author,
+    author_email=author_email,
+    classifiers=classifiers,
+    description=short_desc,
+    install_requires=install_requires,
+    license=license,
+    long_description=long_description,
+    name=modname,
+    packages=packages,
+    py_modules=py_modules,
+    url=web,
+    version=__version__,
+    zip_safe=zip_safe,
+)
