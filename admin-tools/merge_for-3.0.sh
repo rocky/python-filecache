3cca58eea7b87a1d5624e038bf92288ad6e60ae5
rocky <rocky@gnu.org>
2024-10-16 09:43:55 -0400

    Wrong name

--- admin-tools/merge_for-3.0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Setup for running Python 3.0 .. 3.2, merging master into this branch
-pyfilecache_30_owd=$(pwd)
-cd $(dirname ${BASH_SOURCE[0]})
-if . ./setup-python-3.0.sh; then
-    git merge python-3.3-to-3.5
-fi
-cd $pyfilecache_30_owd
