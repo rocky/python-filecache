db69e600e2d09d96ec28d16f705b2311961405ea
rocky <rocky@gnu.org>
2024-10-15 22:35:59 -0400

    Administrivia

--- admin-tools/make-dist-3.0-to-3.2.sh
+++ admin-tools/make-dist-3.0-to-3.2.sh
@@ -3,17 +3,17 @@ PACKAGE=pyficache
 
 # FIXME put some of the below in a common routine
 function finish {
-  cd $make_dist_31_filecache_owd
+  cd $make_dist_30_filecache_owd
 }
 
 cd $(dirname ${BASH_SOURCE[0]})
-make_dist_31_filecache_owd=$(pwd)
+make_dist_30_filecache_owd=$(pwd)
 trap finish EXIT
 
-if ! source ./pyenv-3.1-3.2-versions ; then
+if ! source ./pyenv-3.0-3.2-versions ; then
     exit $?
 fi
-if ! source ./setup-python-3.1.sh ; then
+if ! source ./setup-python-3.0.sh ; then
     exit $?
 fi
 
@@ -47,6 +47,6 @@ done
 python ./setup.py sdist
 tarball=dist/${PACKAGE}-${__version__}.tar.gz
 if [[ -f $tarball ]]; then
-    mv -v $tarball dist/${PACKAGE}_31-${__version__}.tar.gz
+    mv -v $tarball dist/${PACKAGE}_30-${__version__}.tar.gz
 fi
 finish
--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 # Check out master branch and dependent development master branches
+PYTHON_VERSION=3.13
+
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
-
-PYTHON_VERSION=3.12
-
-function checkout_version {
-    local repo=$1
-    version=${2:-python-3.12}
-    echo Checking out $version on $repo ...
-    (cd ../$repo && git checkout $version && pyenv local $PYTHON_VERSION) && \
-	git pull
-    return $?
-}
 
 mydir=$(dirname $bs)
 pyficache_owd=$(pwd)
@@ -23,9 +14,8 @@ cd $mydir
 . ./checkout_common.sh
 fulldir=$(readlink -f $mydir)
 
-(cd $fulldir/.. && checkout_version python-xdis master && \
-    checkout_version shell-term-background master && \
-    git checkout master \
+(cd $fulldir/.. && setup_version python-xdis master && \
+    setup_version shell-term-background master
     )
 cd $python_filecache_owd
 rm -v */.python-version 2>/dev/null || true
