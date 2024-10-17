7c4aa20a5cefe5cebf04a93a42841e3ff5083fc4
rocky <rocky@gnu.org>
2024-10-16 20:17:38 -0400

    Administrivia

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 PYTHON_VERSION=2.4
 
-function checkout_version {
-    local repo=$1
-    version=${2:-python-2.4}
-    echo Checking out $version on $repo ...
-    (cd ../$repo && git checkout $version && pyenv local $PYTHON_VERSION) && \
-	git pull
-    return $?
-}
-
-# FIXME put some of the below in a common routine
-function finish {
-  cd $setup_filecache_24_owd
-}
-
-pyenv local $PYTHON_VERSION
-
 setup_filecache_24_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
@@ -25,7 +9,15 @@ if [[ $0 == $bs ]] ; then
 fi
 
 mydir=$(dirname $bs)
+python_filecache_owd=$(pwd)
+. ./checkout_common.sh
 fulldir=$(readlink -f $mydir)
-(cd $fulldir/.. && checkout_version python-xdis python-2.4-to-2.7 && checkout_version python-filecache python-2.4-to-2.7)
+cd $fulldir/..
+
+(cd $fulldir/.. && setup_version python-xdis python-2.4 && \
+    setup_version shell-term-background python-2.4
+    )
+
+cd $python_filecache_owd
 rm -v */.python-version 2>/dev/null || true
-finish
+checkout_finish python-2.4-to-2.7
